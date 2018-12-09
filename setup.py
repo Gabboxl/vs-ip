4294faeebb95f6ae2ab15b60d85feab5c938ec14
peppelg <me@peppelg.space>
2018-12-09 13:23:51 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='vs-ip',
-    version='0.2',
+    version='0.2.1',
     url='http://github.com/peppelg/vs-ip',
     author='peppelg',
     author_email='me@peppelg.space',
     license='The Unlicense',
-    scripts=['vs-ip.py'],
+    scripts=['vs-ip'],
     install_requires=['splinter'],
     zip_safe=False
 )
